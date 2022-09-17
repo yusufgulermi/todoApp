a7666ce19fc28843afde7078258475afb62cae35
yusufgulermi <my_name0724@hotmail.com>
2022-09-17 19:55:46 +0300

    deploying

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import {BrowserRouter as Router,Routes,Route,} from 'react-router-dom'
+import {HashRouter  as Router,Routes,Route,} from 'react-router-dom'
 import Login from "./Login";
 import TodoList from "./TodoList";
 
